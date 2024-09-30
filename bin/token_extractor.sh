a79ce27a6189f2c93df080104773f4094d281a2f
Michael Schlenstedt <Michael@schlenn.net>
2024-09-30 17:51:22 +0200

    Bugfix Token Extractor

--- bin/token_extractor.sh
+++ bin/token_extractor.sh
@@ -1,1 +1,16 @@
-bash <(curl -L https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/raw/master/run.sh)
+#!/bin/bash
+rm token_extractor.zip*
+rm -r token_extractor
+
+wget https://github.com/PiotrMachowski/Xiaomi-cloud-tokens-extractor/releases/latest/download/token_extractor.zip
+unzip token_extractor.zip
+cd token_extractor
+pip3 install -r requirements.txt
+echo
+echo
+echo "Starting Token Extraktor..."
+echo
+echo
+python3 token_extractor.py
+cd ..
+rm -rf token_extractor token_extractor.zip
